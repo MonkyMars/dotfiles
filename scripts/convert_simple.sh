42ec2a83d2442ed8292e1353c5e813cc081e1e4d
monky <levinoppers@proton.me>
2026-03-20 19:20:57 +0100

    plugins

---
+++ scripts/convert_simple.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+input_file="$1"
+output_file="${input_file%.*}_converted.mkv"
+
+ffmpeg \
+  -hwaccel vaapi \
+  -hwaccel_device /dev/dri/renderD128 \
+  -hwaccel_output_format vaapi \
+  -i "$input_file" \
+  -vf 'scale_vaapi=w=1920:h=-2' \
+  -c:v h264_vaapi \
+  -qp 20 \
+  -c:a aac \
+  -b:a 192k \
+  -map 0:v:0 \
+  -map 0:a:0 \
+  "$output_file"
+
+echo "Done: $output_file"
